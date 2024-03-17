80b03ab309d8e39db064595a855f3ef65889b12f
HariAditya2k3 <122663351+HariAditya2k3@users.noreply.github.com>
2024-03-17 13:53:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import TicTacToe from './Components/Tic Tac Toe/Tic Tac Toe';
+import circle from './Components/Assets/circle.png';
+import cross from './Components/Assets/cross.png';
+const App = () => {
+  return (
+    <div>
+    <TicTacToe/> 
+    </div>
+  )
+}
+
+export default App
